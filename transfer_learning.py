2fc2601f16ed017c28718fd0cea3f44f38e45074
Sadık Barun <75198383+sadikbarun@users.noreply.github.com>
2024-12-24 22:00:19 +0300

    Add files via upload

---
+++ transfer_learning.py
@@ -0,0 +1,220 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import tensorflow as tf
+from sklearn.metrics import roc_auc_score
+
+test_labels = []
+test_predictions = []
+
+# dataset indirme
+_URL = 'https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip'
+path_to_zip = tf.keras.utils.get_file('cats_and_dogs.zip', origin=_URL, extract=True)
+PATH = os.path.join(os.path.dirname(path_to_zip), 'cats_and_dogs_filtered')
+train_dir = os.path.join(PATH, 'train')
+validation_dir = os.path.join(PATH, 'validation')
+
+# train config
+BATCH_SIZE = 32
+IMG_SIZE = (160, 160)
+
+# train ve validation dataset önayar
+train_dataset = tf.keras.utils.image_dataset_from_directory(train_dir,
+                                                            shuffle=True,
+                                                            batch_size=BATCH_SIZE,
+                                                            image_size=IMG_SIZE)
+validation_dataset = tf.keras.utils.image_dataset_from_directory(validation_dir,
+                                                                 shuffle=True,
+                                                                 batch_size=BATCH_SIZE,
+                                                                 image_size=IMG_SIZE)
+
+# data içinde test datası olmadığından validation içinden teste ayırma
+val_batches = tf.data.experimental.cardinality(validation_dataset)
+test_dataset = validation_dataset.take(val_batches // 5)
+validation_dataset = validation_dataset.skip(val_batches // 5)
+
+# datasetleri önceden belleğe alma
+AUTOTUNE = tf.data.AUTOTUNE
+train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
+validation_dataset = validation_dataset.prefetch(buffer_size=AUTOTUNE)
+test_dataset = test_dataset.prefetch(buffer_size=AUTOTUNE)
+
+# halihazırdaki datasetten yeni datalar üretmek için data artırma yöntemi ayarı
+data_augmentation = tf.keras.Sequential([
+  tf.keras.layers.RandomFlip('horizontal'),
+  tf.keras.layers.RandomRotation(0.2),
+])
+
+preprocess_input = tf.keras.applications.mobilenet_v2.preprocess_input
+rescale = tf.keras.layers.Rescaling(1./127.5, offset=-1)
+
+# Create the base model from the pre-trained model MobileNet V2
+IMG_SHAPE = IMG_SIZE + (3,)
+print("IMG_SHAPE: ",IMG_SHAPE)
+base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
+                                               include_top=False,
+                                               weights='imagenet')
+
+image_batch, label_batch = next(iter(train_dataset))
+feature_batch = base_model(image_batch)
+print(feature_batch.shape)
+
+base_model.trainable = False
+
+global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+feature_batch_average = global_average_layer(feature_batch)
+
+prediction_layer = tf.keras.layers.Dense(1, activation='sigmoid')
+prediction_batch = prediction_layer(feature_batch_average)
+
+inputs = tf.keras.Input(shape=(160, 160, 3))
+x = data_augmentation(inputs)
+x = preprocess_input(x)
+x = base_model(x, training=False)
+x = global_average_layer(x)
+x = tf.keras.layers.Dropout(0.2)(x)
+outputs = prediction_layer(x)
+model = tf.keras.Model(inputs, outputs)
+print("model trainable variables: ",len(model.trainable_variables))
+
+base_learning_rate = 0.0001
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=base_learning_rate),
+              loss=tf.keras.losses.BinaryCrossentropy(),
+              metrics=[tf.keras.metrics.BinaryAccuracy(threshold=0.5, name='accuracy')])
+
+initial_epochs = 10
+loss0, accuracy0 = model.evaluate(validation_dataset)
+print("initial loss: {:.2f}".format(loss0))
+print("initial accuracy: {:.2f}".format(accuracy0))
+
+# training
+history = model.fit(train_dataset,
+                    epochs=initial_epochs,
+                    validation_data=validation_dataset)
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+model.save('C:/Users/sadba/OneDrive/Masaüstü/YL_courses/pattern/HW_2/cats_and_dogs_classifier_base.keras')
+
+plt.figure(figsize=(8, 8))
+plt.subplot(2, 1, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.ylabel('Accuracy')
+plt.ylim([min(plt.ylim()),1])
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 1, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.ylabel('Cross Entropy')
+plt.ylim([0,1.0])
+plt.title('Training and Validation Loss')
+plt.xlabel('epoch')
+# plt.show()
+plt.savefig('first_training_validation_accuracy_loss.jpg')
+plt.close()
+
+# Retrieve a batch of images from the test set
+image_batch0, label_batch0 = test_dataset.as_numpy_iterator().next()
+predictions0 = model.predict_on_batch(image_batch0).flatten()
+predictions0 = tf.where(predictions0 < 0.5, 0, 1)
+
+print('Predictions:\n', predictions0.numpy())
+print('Labels:\n', label_batch0)
+
+
+for images, labels in test_dataset:
+    preds = model.predict(images)
+    test_labels.extend(labels.numpy())
+    test_predictions.extend(preds.flatten())
+
+auc_score = roc_auc_score(test_labels, test_predictions)
+print("Test AUC Score:", auc_score)
+print("\n", "*-"*20, " FINETUNE ", "*-"*20, "\n")
+
+# Finetuning
+base_model.trainable = True
+
+# Let's take a look to see how many layers are in the base model
+print("Number of layers in the base model: ", len(base_model.layers))
+
+# Fine-tune from this layer onwards
+fine_tune_at = 100
+
+# Freeze all the layers before the `fine_tune_at` layer
+for layer in base_model.layers[:fine_tune_at]:
+  layer.trainable = False
+
+model.compile(loss=tf.keras.losses.BinaryCrossentropy(),
+              optimizer = tf.keras.optimizers.RMSprop(learning_rate=base_learning_rate/10),
+              metrics=[tf.keras.metrics.BinaryAccuracy(threshold=0.5, name='accuracy')])
+
+print("model trainable variables: ",len(model.trainable_variables))
+fine_tune_epochs = 10
+total_epochs =  initial_epochs + fine_tune_epochs
+
+# training
+history_fine = model.fit(train_dataset,
+                         epochs=total_epochs,
+                         initial_epoch=len(history.epoch),
+                         validation_data=validation_dataset)
+
+acc += history_fine.history['accuracy']
+val_acc += history_fine.history['val_accuracy']
+
+loss += history_fine.history['loss']
+val_loss += history_fine.history['val_loss']
+
+model.save('C:/Users/sadba/OneDrive/Masaüstü/YL_courses/pattern/HW_2/cats_and_dogs_classifier_finetune.keras')
+
+plt.figure(figsize=(8, 8))
+plt.subplot(2, 1, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.ylim([0.8, 1])
+plt.plot([initial_epochs-1,initial_epochs-1],
+          plt.ylim(), label='Start Fine Tuning')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 1, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.ylim([0, 1.0])
+plt.plot([initial_epochs-1,initial_epochs-1],
+         plt.ylim(), label='Start Fine Tuning')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.xlabel('epoch')
+# plt.show()
+plt.savefig('Finetune_training_validation_accuracy_loss.jpg')
+plt.close()
+
+loss, accuracy = model.evaluate(test_dataset)
+print('Test accuracy :', accuracy)
+
+# Retrieve a batch of images from the test set
+image_batch, label_batch = test_dataset.as_numpy_iterator().next()
+predictions = model.predict_on_batch(image_batch).flatten()
+predictions = tf.where(predictions < 0.5, 0, 1)
+
+print('Predictions:\n', predictions.numpy())
+print('Labels:\n', label_batch)
+
+test_labels1 = []
+test_predictions1 = []
+
+for images, labels in test_dataset:
+    preds1 = model.predict(images)
+    test_labels1.extend(labels.numpy())
+    test_predictions1.extend(preds1.flatten())
+
+auc_score1 = roc_auc_score(test_labels1, test_predictions1)
+print("Test AUC Score Finetune:", auc_score1)
